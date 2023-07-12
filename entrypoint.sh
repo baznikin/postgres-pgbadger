6614189e44996764240817330119f293c0775db9
Andrey Baznikin <a.baznikin@gmail.com>
2023-07-12 14:11:08 +0400

    Process 2 files; output file info

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,21 +13,23 @@ mkdir -p "$PGBADGER_DATA"
 
 run_pgbadger () {
 
+  CMD=$@ ; export CMD
   if [ "$(ls -A $PGBADGER_DATA)" ]; then
-    echo "PGBADGER_DATA $PGBADGER_DATA contains data, try to process latest file"
-    ls -1tr ${PGLOGS}/*.${LOG_FORMAT} | tail -1 | xargs -n1 $@ --outdir "$PGBADGER_DATA" --format "$LOG_FORMAT" --incremental --start-monday
+    echo "PGBADGER_DATA $PGBADGER_DATA contains data, try to process latest 2 files"
+    # If previous run was before midnight, then some records contains in previous log, some - in current. So, we always process both
+    ls -1tr ${PGLOGS}/*.${LOG_FORMAT} | tail -2 | xargs -n1 -I % sh -c '{ ls -l %; ${CMD} --outdir "$PGBADGER_DATA" --format "$LOG_FORMAT" --incremental --start-monday %; }'
   else
     echo "PGBADGER_DATA $PGBADGER_DATA is empty, try to process all files"
-    ls -1tr ${PGLOGS}/*.${LOG_FORMAT} | xargs -n1 $@ --outdir "$PGBADGER_DATA" --format "$LOG_FORMAT" --incremental --start-monday
+    ls -1tr ${PGLOGS}/*.${LOG_FORMAT} | xargs -n1 -I % sh -c '{ ls -l %; ${CMD} --outdir "$PGBADGER_DATA" --format "$LOG_FORMAT" --incremental --start-monday %; }'
   fi
 
   return 0
 }
 
-if [[ 1 == "${CRON}" ]]; then
-
-  echo "run $CRON_PATTERN"
-  echo "$CRON_PATTERN /entrypoint.sh $@" > /etc/crontabs/root
+if [[ "true" == "${CRON}" || "1" == "${CRON}" ]]; then
+  env | sort
+  echo "run $CRON_SCHEDULE"
+  echo "$CRON_SCHEDULE /entrypoint.sh $@" > /etc/crontabs/root
   export CRON=0
   crond -f
 
